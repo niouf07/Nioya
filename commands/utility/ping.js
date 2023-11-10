924646483b5b563f109a88dd608d28efc5b0879f
Niouf <contact@niouf.net>
2023-11-10 12:03:59 +0100

    Ping marche pas

---
+++ commands/utility/ping.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require("discord.js");
+const wait = require("node:timers/promises").setTimeout;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with the bot ping!"),
+
+  async execute(client, interaction) {
+    await interaction.deferReply();
+
+    await wait(4000);
+
+    const reply = await interaction.fetchReply();
+
+    const ping = reply.createdTimestamp - interaction.createdTimestamp;
+
+    interaction.editReply(
+      `Pong! Client ${ping}ms | Websocket: ${client.ws.ping}ms`
+    );
+  },
+};
